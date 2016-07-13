e4c8aaf52e30341b009e360806b08f128371cf91
pauky <yangruiwei@pintu360.com>
2016-07-13 17:15:44 +0800

    config fix

--- conf/config.js
+++ conf/config.js
@@ -24,7 +24,7 @@ var config = {
         portalDomain: "127.0.0.1",
         cookieDomain: "127.0.0.1",
         staticDomain: "http://127.0.0.1:3100/blog/",
-        staticRoot: 'D:/pauky/work/blog-portal-static',// 静态资源路径,个人本地路径(办公室)
+        staticRoot: 'D:/pauky/work/yrw-blog-static',// 静态资源路径,个人本地路径(办公室)
         bindIp: "0.0.0.0",
         httpPort: 3100,
         email: {
