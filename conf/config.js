356185880005c49c01388570230436db9ab39a45
pauky <yangruiwei@pintu360.com>
2016-07-13 17:04:20 +0800

    config fix

--- conf/config.js
+++ conf/config.js
@@ -9,48 +9,10 @@ var node_env = nconf.get('NODE_ENV') || 'dev';
 
 var config = {
   prd: {
-    siteName: '行之足',
-    mongo: {
-      connectionStr: 'mongodb://blog:xingzhizuadmin%^&*@112.74.211.18:27017/blog?autoReconnect=true&bufferMaxEntries=0'
-    },
-    portalUrl: "https://www.yangrunwei.com",
-    portalDomain: "www.yangrunwei.com",
-    cookieDomain: "www.yangrunwei.com",
-    staticDomain: "https://static.yangrunwei.com/blog/",
-    bindIp: '0.0.0.0',
-    httpPort: 3100,
-    uploadImageDomain: "https://img.yangrunwei.com/",
-    uploadFileDomain: 'http://file.yangrunwei.com/',
-    email: {
-      auth: {
-        session_secret: 'yangrunwei-love-blog!@#',
-        user: 'glowrypauky@163.com',
-        pass: 'glowrypauky918'
-      }
-    },
-    logStdout: false
+
   },
   sit: {
-    siteName: '行之足',
-    mongo: {
-      connectionStr: "mongodb://blog:abc123@192.168.1.101:27017/blog?autoReconnect=true&bufferMaxEntries=0"
-    },
-    portalUrl: "http://sit.yangrunwei.com",
-    portalDomain: "sit.yangrunwei.com",
-    cookieDomain: "sit.yangrunwei.com",
-    staticDomain: "http://sit.static.yangrunwei.com/blog/",
-    bindIp: '0.0.0.0',
-    httpPort: 3100,
-    uploadImageDomain: "http://img.yangrunwei.com/",
-    uploadFileDomain: 'http://file.yangrunwei.com/',
-    email: {
-      auth: {
-        session_secret: 'yangrunwei-love-blog!@#',
-        user: 'glowrypauky@163.com',
-        pass: 'glowrypauky918'
-      }
-    },
-    logStdout: false
+
   },
 
   dev: {
@@ -67,13 +29,11 @@ var config = {
     staticRoot: 'D:/pauky/work/blog-portal-static',// 静态资源路径,个人本地路径(办公室)
     bindIp: "0.0.0.0",
     httpPort: 3100,
-    uploadImageDomain: "http://img.yangrunwei.com/",
-    uploadFileDomain: 'http://file.yangrunwei.com/',
     email: {
       auth: {
-        session_secret: 'yangrunwei-love-blog!@#',
-        user: 'glowrypauky@163.com',
-        pass: 'glowrypauky918'
+        session_secret: 'your session_secret',
+        user: 'your email',
+        pass: 'your password'
       }
     },
     logStdout: true
